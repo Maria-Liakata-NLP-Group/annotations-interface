ea787c022495a0faaae0209b71a5f546ab7f4da5
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-05-04 13:42:58 +0100

    new login view function in routes module

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,17 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
 @app.route('/index')
 def index():
+    """This is the index page"""
     user = {"email": "random.user@random.com"}
     return render_template('index.html', user=user)
+
+@app.route('/login')
+def login():
+    """This is the login page"""
+    form = LoginForm()
+    return render_template('login.html', title="Sign In", form=form)
