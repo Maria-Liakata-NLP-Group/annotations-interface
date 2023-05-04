d9461e35d2a3338621645cc63a3eb6a2dc585214
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-05-04 22:23:04 +0100

    add length validator to password field

--- app/forms.py
+++ app/forms.py
@@ -1,13 +1,13 @@
 # Desc: Forms for the app
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, Length
 
 
 class LoginForm(FlaskForm):
     """Login form - inherits from FlaskForm"""
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])
     submit = SubmitField('Sign In')
     remember_me = BooleanField('Remember Me')
 
