4c4bb6669829b18fe8c0b7b831b3cbaab9755061
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-05-04 22:05:36 +0100

    update login view function to receive form data

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
@@ -10,8 +10,15 @@ def index():
     user = {"email": "random.user@random.com"}
     return render_template('index.html', user=user)
 
-@app.route('/login')
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     """This is the login page"""
     form = LoginForm()
+    if form.validate_on_submit():
+        # This only runs if the form is submitted (`POST`) and validated
+        # flash() registers a message to show to the user
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.email.data, form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html', title="Sign In", form=form)
