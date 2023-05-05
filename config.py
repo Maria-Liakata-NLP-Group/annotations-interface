ce3e9a97046444103f8f80168997fef5c1815f4d
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-05-05 11:13:29 +0100

    use os.environ.get()

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class BaseConfig(object):
     """Base configuration. Configuration settings are defined here."""
-    SECRET_KEY = os.environ["FLASK_APP_SECRET_KEY"]
-    SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"] or \
+    SECRET_KEY = os.environ.get("FLASK_APP_SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
