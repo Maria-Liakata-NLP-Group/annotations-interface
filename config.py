71cf1e73effcfb83d1f7ca6817e72bb898df9350
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-05-10 10:21:09 +0100

    change env variable name for secret key

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class BaseConfig(object):
     """Base configuration. Configuration settings are defined here."""
-    SECRET_KEY = os.environ.get("FLASK_APP_SECRET_KEY")
+    SECRET_KEY = os.environ.get("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
