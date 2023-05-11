07dbff7f86751be427ba131803545f6f1948adc2
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-05-11 16:27:27 +0100

    allow different users to have the same email address

--- app/models.py
+++ app/models.py
@@ -25,7 +25,7 @@ class User(UserMixin, db.Model):
     username = db.Column(
         db.String(64), index=True, unique=True
     )  # index=True - for faster search
-    email = db.Column(db.String(120), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=False)
     password_hash = db.Column(db.String(128))  # hash of password
     posts = db.relationship(
         "Post", backref="author", lazy="dynamic"
