a644fe4692caffba407be66881f17c3aa9af6a52
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-05-22 14:25:53 +0100

    update shell context for flask shell

--- annotations_interface.py
+++ annotations_interface.py
@@ -1,8 +1,14 @@
 from app import app, db
-from app.models import User, SMAnnotation, SMPost
+from app.models import User, SMAnnotation, SMPost, SMReply
 
 
 # Create shell context for flask shell
 @app.shell_context_processor
 def make_shell_context():
-    return {"db": db, "User": User, "SMAnnotation": SMAnnotation, "SMPost": SMPost}
+    return {
+        "db": db,
+        "User": User,
+        "SMAnnotation": SMAnnotation,
+        "SMPost": SMPost,
+        "SMReply": SMReply,
+    }
