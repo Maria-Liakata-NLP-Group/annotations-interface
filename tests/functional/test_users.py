e13189cbd249bb56fab33511db7369da9acf974f
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-05-23 20:11:53 +0100

    new functional test for user login/logout

--- tests/functional/test_users.py
+++ tests/functional/test_users.py
@@ -12,3 +12,32 @@ def test_login_page(test_client):
     assert b"Remember Me" in response.data
     assert b"New User?" in response.data
     assert b"Click to Register" in response.data
+
+
+def test_valid_login_logout(test_client, init_database):
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/login' page is posted to (POST) with valid credentials
+    THEN check the response is valid
+    """
+    response = test_client.post(
+        "/auth/login",
+        data={"username": "test1", "password": "testpassword1"},
+        follow_redirects=True,
+    )
+    assert response.status_code == 200
+    assert b"Logout" in response.data
+    assert b"test1" in response.data
+    assert b"New User?" not in response.data
+    assert b"Click to Register" not in response.data
+    assert b"Sign In" not in response.data
+
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/logout' page is requested (GET)
+    THEN check the response is valid
+    """
+    response = test_client.get("/auth/logout", follow_redirects=True)
+    assert response.status_code == 200
+    assert b"Sign In" in response.data
+    assert b"test1" not in response.data
