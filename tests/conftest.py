c7a83c143458b23d634cbc2fb21ff83fa8dcfd27
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-05-23 17:54:23 +0100

    pytest fixture to create a test client

--- tests/conftest.py
+++ tests/conftest.py
@@ -32,3 +32,15 @@ def new_sm_reply(new_sm_post):
     """Fixture to create a new social media reply"""
     reply = SMReply(comment="test reply", post=new_sm_post)
     return reply
+
+
+@pytest.fixture(scope="module")
+def test_client():
+    """Fixture to create a test client"""
+    # Create the Flask app configured for testing
+    flask_app = create_app(TestConfig)
+    # Create a test client using the Flask application configured for testing
+    with flask_app.test_client() as testing_client:
+        # Establish an application context before running the tests
+        with flask_app.app_context():
+            yield testing_client  # this is where the testing happens!
