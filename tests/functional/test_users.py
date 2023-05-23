a640133e5cd78d5b5ef018a7ec8718b2707b0f65
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-05-23 20:21:00 +0100

    new functional test for invalid user login

--- tests/functional/test_users.py
+++ tests/functional/test_users.py
@@ -41,3 +41,22 @@ def test_valid_login_logout(test_client, init_database):
     assert response.status_code == 200
     assert b"Sign In" in response.data
     assert b"test1" not in response.data
+
+
+def test_invalid_login(test_client, init_database):
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/login' page is posted to (POST) with invalid credentials
+    THEN check an error message is returned to the user
+    """
+    response = test_client.post(
+        "auth/login",
+        data={"username": "test1", "password": "wrongpassword"},
+        follow_redirects=True,
+    )
+    assert response.status_code == 200
+    assert b"Invalid username or password" in response.data
+    assert b"Sign In" in response.data
+    assert b"test1" not in response.data
+    assert b"New User?" in response.data
+    assert b"Click to Register" in response.data
