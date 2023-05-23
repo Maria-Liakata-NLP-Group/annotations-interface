c12913721ce6f7feffbc18b52c8b57a10b30bc58
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-05-23 17:58:25 +0100

    functional test for login page

---
+++ tests/functional/test_users.py
@@ -0,0 +1,14 @@
+def test_login_page(test_client):
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/login' page is requested (GET)
+    THEN check the response is valid
+    """
+    response = test_client.get("/auth/login")
+    assert response.status_code == 200
+    assert b"Sign In" in response.data
+    assert b"Username" in response.data
+    assert b"Password" in response.data
+    assert b"Remember Me" in response.data
+    assert b"New User?" in response.data
+    assert b"Click to Register" in response.data
