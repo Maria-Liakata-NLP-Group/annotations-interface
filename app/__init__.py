32be0cad93076de2be12ebb9d94bf55890a0f179
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-05-26 17:11:40 +0100

    fix migration errors associated with ALTER tables

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,22 @@ from config import BaseConfig
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from sqlalchemy import MetaData
+
+# custom metadata naming convention
+# this is required for dealing with alembic migrations
+# see: https://flask-sqlalchemy.palletsprojects.com/en/2.x/config/#using-custom-metadata-and-naming-conventions
+naming_convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+metadata = MetaData(naming_convention=naming_convention)
 
 # extension instances (global) not bound to application
-db = SQLAlchemy()  # database instance
+db = SQLAlchemy(metadata=metadata)  # database instance
 migrate = Migrate()  # migration engine instance
 login = LoginManager()  # login manager instance
 login.login_view = "auth.login"  # login view function (endpoint) name
@@ -25,7 +38,8 @@ def create_app(config_class=BaseConfig):
 
     # bind extensions to application
     db.init_app(app)
-    migrate.init_app(app, db)
+    # see: https://blog.miguelgrinberg.com/post/fixing-alter-table-errors-with-flask-migrate-and-sqlite
+    migrate.init_app(app, db, render_as_batch=True)
     login.init_app(app)
 
     # register blueprints
