08c5dea8d3b5dc6292157f1d8cf4225f688acfae
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-05-30 17:29:09 +0100

    fix dataset name validator in UploadForm

--- app/upload/forms.py
+++ app/upload/forms.py
@@ -9,17 +9,21 @@ class UploadForm(FlaskForm):
     """Dataset upload form for the app"""
 
     name = StringField(
-        "Dataset name", validators=[DataRequired(), Length(min=5, max=50)]
+        "Dataset name", validators=[DataRequired(), Length(min=4, max=50)]
     )
     description = TextAreaField(
         "Please provide a short description",
-        validators=[DataRequired(), Length(min=5, max=100)],
+        validators=[DataRequired(), Length(min=5, max=200)],
     )
     file = FileField("File", validators=[DataRequired()])
     submit = SubmitField("Upload dataset")
 
-    def validate_dataset(self, name):
-        """Validate the dataset name"""
+    def validate_name(self, name):
+        """
+        Validate the dataset name.
+        This is a custom validator, and for it to work, the function name must be
+        in the format validate_<field_name>.
+        """
         file_name = Dataset.query.filter_by(name=name.data).first()
         if file_name is not None:
             raise ValidationError(
