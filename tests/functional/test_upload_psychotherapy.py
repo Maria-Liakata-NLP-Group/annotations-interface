524054b5ec379e8fc6c3b3cb51d614db2b7445bb
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-06-26 13:29:16 +0100

    functional tests for psychotherapy session data upload

--- tests/functional/test_upload_psychotherapy.py
+++ tests/functional/test_upload_psychotherapy.py
@@ -1,26 +1,27 @@
 """
 Functional tests for the upload (`upload`) blueprint.
-Social media dataset upload page.
+Psychotherapy session dataset upload page.
 """
-from app.models import User, Dataset, SMPost, SMReply
+from app.models import User, Dataset, Psychotherapy
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 
-def test_upload_sm_login_required(test_client):
+def test_upload_psychotherapy_login_required(test_client):
     """
     GIVEN a Flask application configured for testing
-    WHEN the '/upload_sm' page is requested (GET) without being logged in
+    WHEN the '/upload_psychotherapy' page is requested (GET) without being logged in
     THEN check the response is valid and the user is redirected to the login page
     """
-    response = test_client.get("/upload/upload_sm")
+    response = test_client.get("/upload/upload_psychotherapy")
     assert response.status_code == 302  # check redirect
     assert "/auth/login" in response.headers["Location"]
 
 
-def test_upload_sm_valid_login(test_client, init_database):
+def test_upload_psychotherapy_valid_login(test_client, init_database):
     """
     GIVEN a Flask application configured for testing
-    WHEN the '/upload_sm' page is requested (GET) after logging in
+    WHEN the '/upload_psychotherapy' page is requested (GET) after logging in
     THEN check the response is valid
     """
     # log in to the app
@@ -32,9 +33,9 @@ def test_upload_sm_valid_login(test_client, init_database):
     assert response.status_code == 200
 
     # check upload page
-    response = test_client.get("/upload/upload_sm")
+    response = test_client.get("/upload/upload_psychotherapy")
     assert response.status_code == 200
-    assert b"Upload new social media dataset" in response.data
+    assert b"Upload new psychotherapy session dataset" in response.data
     assert b"Dataset name" in response.data
     assert b"description" in response.data
     assert b'type="submit"' in response.data  # check submit button
@@ -45,10 +46,10 @@ def test_upload_sm_valid_login(test_client, init_database):
     assert response.status_code == 200
 
 
-def test_upload_sm_valid_dataset(test_client, init_database):
+def test_upload_psychotherapy_valid_dataset(test_client, init_database):
     """
     GIVEN a Flask application configured for testing
-    WHEN the '/upload_sm' page is requested (POST) with a valid dataset
+    WHEN the '/upload_psychotherapy' page is requested (POST) with a valid dataset
     THEN check the response is valid and the dataset is added to the database correctly
     """
     # log in to the app
@@ -60,18 +61,18 @@ def test_upload_sm_valid_dataset(test_client, init_database):
     assert response.status_code == 200
 
     # check upload page
-    response = test_client.get("/upload/upload_sm")
+    response = test_client.get("/upload/upload_psychotherapy")
     assert response.status_code == 200
 
     # upload a valid dataset
-    with open("tests/data/timelines_example_lorem.pickle", "rb") as handle:
+    with open("tests/data/psychotherapy_example_lorem.pickle", "rb") as handle:
         response = test_client.post(
-            "/upload/upload_sm",
+            "/upload/upload_psychotherapy",
             data={
                 "name": "test_dataset",
                 "description": "test description",
                 "annotator": User.query.filter_by(username="admin1").first().id,
-                "file": (handle, "timelines_example_lorem.pickle"),
+                "file": (handle, "psychotherapy_example_lorem.pickle"),
             },
             follow_redirects=True,
         )
@@ -91,31 +92,23 @@ def test_upload_sm_valid_dataset(test_client, init_database):
     assert dataset.description == "test description"
     assert dataset.id_author == user.id
     assert dataset.id_annotator == user.id
-    assert dataset.type.value == "Social Media Thread"
+    assert dataset.type.value == "Psychotherapy Session"
 
-    posts = SMPost.query.filter_by(id_dataset=dataset.id).all()
-    assert len(posts) == 43
-    replies = SMReply.query.filter_by(id_dataset=dataset.id).all()
-    assert len(replies) == 92
-
-    # more granular checks
-    posts = SMPost.query.filter_by(
-        user_id="746731", timeline_id="746731_1", id_dataset=dataset.id
-    ).all()
-    assert len(posts) == 10
-
-    post = SMPost.query.filter_by(
-        user_id="746731", timeline_id="746731_1", post_id=9602529, id_dataset=dataset.id
-    ).first()
-    assert (post.mood).lower() == "happy"
-    replies = SMReply.query.filter_by(id_sm_post=post.id).all()
-    assert len(replies) == 2
+    psychotherapy = Psychotherapy.query.filter_by(id_dataset=dataset.id).all()
+    assert len(psychotherapy) == 228
+    psychotherapy[
+        100
+    ].event_id == 100  # event id is the row index in the original dataframe
+    psychotherapy[100].id == 101  # this is the row index of the SQL table
+    psychotherapy[100].event_text == "Amet consectetur quiquia dolor sit."
+    psychotherapy[100].event_speaker == "Client"
+    psychotherapy[100].date == datetime.strptime("6/18/2015", "%m/%d/%Y").date()
 
 
 def test_upload_based_on_role(test_client, init_database):
     """
     GIVEN a Flask application configured for testing
-    WHEN the '/upload_sm' page is requested (GET) after logging in as a user with a role
+    WHEN the '/upload_psychotherapy' page is requested (GET) after logging in as a user with a role
     THEN check the 'Annotator' SelectField is populated with the correct users
     """
     # log in to the app as a user with the 'annotator' role
@@ -127,7 +120,7 @@ def test_upload_based_on_role(test_client, init_database):
     assert response.status_code == 200
 
     # check upload page
-    response = test_client.get("/upload/upload_sm")
+    response = test_client.get("/upload/upload_psychotherapy")
     assert response.status_code == 200
 
     soup = BeautifulSoup(response.data, "html.parser")  # parse HTML
@@ -153,7 +146,7 @@ def test_upload_based_on_role(test_client, init_database):
     assert response.status_code == 200
 
     # check upload page
-    response = test_client.get("/upload/upload_sm")
+    response = test_client.get("/upload/upload_psychotherapy")
     assert response.status_code == 200
 
     soup = BeautifulSoup(response.data, "html.parser")  # parse HTML
