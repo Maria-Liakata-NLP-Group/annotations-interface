68769c2167a245b838cf447de061d419f921a441
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-06-29 14:36:06 +0100

    home page now shows assigned datasets

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,6 @@
 from app.main import bp
 from flask import render_template
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 
 @bp.route("/")
@@ -8,4 +8,6 @@ from flask_login import login_required
 @login_required  # This decorator ensures that the user is logged in before accessing the page
 def index():
     """This is the index page"""
-    return render_template("index.html", title="Home page")
+    # find all the datasets that the logged in user has access to
+    datasets = current_user.datasets.all()
+    return render_template("index.html", title="Home page", datasets=datasets)
