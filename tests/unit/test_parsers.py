533eeb4fff761ac18b4d4cc4484a6ea010bb4eb8
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-07-06 14:36:03 +0100

    more tests for dataset parsers

--- tests/unit/test_parsers.py
+++ tests/unit/test_parsers.py
@@ -1,6 +1,7 @@
 # unit tests for parsers.py of the upload blueprint
-from app.upload.parsers import read_pickle, sm_dict_to_sql
-from app.models import Dataset, SMPost, SMReply
+from app.upload.parsers import read_pickle, sm_dict_to_sql, psychotherapy_df_to_sql
+from app.models import Dataset, SMPost, SMReply, PSDialogTurn, PSDialogEvent
+from datetime import datetime
 
 
 def test_sm_dict_to_sql(flask_app, db_session, insert_datasets):
@@ -31,3 +32,35 @@ def test_sm_dict_to_sql(flask_app, db_session, insert_datasets):
     assert (post.mood).lower() == "happy"
     replies = SMReply.query.filter_by(id_sm_post=post.id).all()
     assert len(replies) == 2
+
+
+def test_psychotherapy_df_to_sql(flask_app, db_session):
+    """
+    Test the psychotherapy_df_to_sql function,
+    which converts the psychotherapy dataframe to SQL and adds it to the database.
+    """
+    df = read_pickle(flask_app.config["PS_DATASET_PATH"])
+    dataset = Dataset.query.filter_by(name="Psychotherapy Dataset Test").first()
+    psychotherapy_df_to_sql(df, dataset)
+    db_session.commit()
+
+    # check that the parsing is performed correctly and the data is added to the database
+    dialog_turns = PSDialogTurn.query.filter_by(id_dataset=dataset.id).all()
+    dialog_events = PSDialogEvent.query.filter_by(id_dataset=dataset.id).all()
+
+    # check that the number of dialog turns and dialog events is correct
+    # a Timestamp event marks the start of a dialog turn
+    assert len(dialog_turns) == sum(df["event_speaker"] == "Timestamp")
+    assert len(dialog_events) == len(df) - sum(df["event_speaker"] == "Timestamp")
+
+    # fetch the dialog turn with dialog_turn_n = 3, i.e. fourth dialog turn
+    dialog_turn = PSDialogTurn.query.filter_by(
+        id_dataset=dataset.id, dialog_turn_n=3
+    ).all()
+
+    expected_timestamp = datetime.strptime(
+        (df.loc[13, "event_plaintext"]).replace(" ", ""), ("%H:%M:%S")
+    ).time()
+
+    assert len(dialog_turn) == 1  # check that only one dialog turn is returned
+    assert dialog_turn[0].timestamp == expected_timestamp
