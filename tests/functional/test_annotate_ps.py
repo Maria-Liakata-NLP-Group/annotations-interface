d1467cf68fa414f9aac720bcb128db268b0d4bfd
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-07-11 16:05:20 +0100

    improve functional tests for psy annotation page

--- tests/functional/test_annotate_ps.py
+++ tests/functional/test_annotate_ps.py
@@ -2,15 +2,15 @@
 Functional tests for the `annotate` blueprint.
 Psychotherapy session dataset annotation page.
 """
-from app.models import Dataset
 from flask_login import current_user
+from bs4 import BeautifulSoup
 
 
-def test_annotate_ps_valid_login(test_client, insert_users, insert_datasets):
+def test_annotate_ps_valid_login(test_client, insert_users, insert_ps_dialog_turns):
     """
-    GIVEN a Flask application configured for testing
+    GIVEN a Flask application configured for testing and a dataset with psychotherapy dialog turns
     WHEN the '/annotate_psychotherapy' page is requested (GET) after logging in
-    THEN check the response is valid
+    THEN check the response is valid and contains expected data
     """
     # log in to the app
     response = test_client.post(
@@ -24,8 +24,32 @@ def test_annotate_ps_valid_login(test_client, insert_users, insert_datasets):
     dataset = current_user.datasets.filter_by(name="Psychotherapy Dataset Test").all()
     dataset_id = dataset[0].id
 
-    # check annotation page for this dataset
+    # check the first annotation page for this dataset
     response = test_client.get("/annotate/annotate_psychotherapy/" + str(dataset_id))
     assert response.status_code == 200
     assert b"Annotating psychotherapy session" in response.data
     assert b"Psychotherapy Dataset Test" in response.data
+    assert b"Client:" in response.data
+    assert b"Therapist:" in response.data
+    # check that the pager is present
+    soup = BeautifulSoup(response.data, "html.parser")
+    pager = soup.find("ul", class_="pager")
+    assert pager is not None
+    previous_button = pager.find("li", class_="previous")
+    assert previous_button is not None
+    next_button = pager.find("li", class_="next")
+    assert next_button is not None
+
+    # move to the next page
+    response = test_client.get(
+        "/annotate/annotate_psychotherapy/" + str(dataset_id) + "?page=2"
+    )
+    assert response.status_code == 200
+    assert b"Annotating psychotherapy session" in response.data
+    assert b"Psychotherapy Dataset Test" in response.data
+    assert b"Client:" in response.data
+    assert b"Therapist:" in response.data
+
+    # log out
+    response = test_client.get("/auth/logout", follow_redirects=True)
+    assert response.status_code == 200
