3489d9725129ed59bf9f85d140999f5268f47e03
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-07-20 14:49:46 +0100

    annotate blueprint - annotations at dialog turn level

--- app/annotate/routes.py
+++ app/annotate/routes.py
@@ -1,7 +1,10 @@
 from app.annotate import bp
+from app.annotate.forms import PSAnnotationForm
+from app import db
 from flask import render_template, request, url_for, current_app
-from flask_login import login_required
-from app.models import Dataset
+from flask_login import login_required, current_user
+from app.models import Dataset, PSDialogTurnAnnotation
+from app.utils import Speaker
 from app.annotate.utils import split_dialog_turns, get_events_from_sections
 
 
@@ -35,7 +38,36 @@ def get_page_items(page, events, dataset_id):
     return page_items, next_url, prev_url, first_url, last_url, total_pages
 
 
-@bp.route("/annotate_psychotherapy/<int:dataset_id>")
+def new_dialog_turn_annotation_to_db(form, speaker, dataset_id, dialog_turn_ids):
+    """
+    Create a new psychotherapy dialog turn annotation object and add it to the database session.
+    Loops through the dialog turn IDs and creates a new annotation for each one.
+    """
+    for dialog_turn_id in dialog_turn_ids:
+        dialog_turn_annotation = PSDialogTurnAnnotation(
+            label_a=form.label_a.data,
+            label_b=form.label_b.data,
+            label_c=form.label_c.data,
+            label_d=form.label_d.data,
+            label_e=form.label_e.data,
+            strength_a=form.strength_a.data,
+            strength_b=form.strength_b.data,
+            strength_c=form.strength_c.data,
+            strength_d=form.strength_d.data,
+            comment_a=form.comment_a.data,
+            comment_b=form.comment_b.data,
+            comment_c=form.comment_c.data,
+            comment_d=form.comment_d.data,
+            comment_e=form.comment_e.data,
+            speaker=speaker,
+            id_user=current_user.id,
+            id_ps_dialog_turn=dialog_turn_id,
+            id_dataset=dataset_id,
+        )
+        db.session.add(dialog_turn_annotation)
+
+
+@bp.route("/annotate_psychotherapy/<int:dataset_id>", methods=["GET", "POST"])
 @login_required
 def annotate_ps(dataset_id):
     """This is the annotations page for psychotherapy datasets"""
@@ -45,17 +77,14 @@ def annotate_ps(dataset_id):
     dialog_turns = dataset.dialog_turns.order_by(
         "timestamp"
     ).all()  # fetch all dialog turns associated with the dataset
-    # split the dialog turns into sections of five minutes
-    app_config = current_app.config  # Get the app config
     try:
+        # split the dialog turns into sections of a given time interval (specified in the app config)
+        app_config = current_app.config  # Get the app config
         sections = split_dialog_turns(
             dialog_turns, time_interval=app_config["PS_MINS_PER_PAGE"] * 60
         )
         # get the events corresponding to each section
         events = get_events_from_sections(sections)  # a list of lists containing events
-        start_times = [
-            section[0].timestamp for section in sections
-        ]  # the starting times of each section
         # get the page number from the request
         page = request.args.get("page", 1, type=int)  # default page is 1
         (
@@ -68,7 +97,38 @@ def annotate_ps(dataset_id):
         ) = get_page_items(
             page, events, dataset_id
         )  # get the events for the current page and the urls for the pager
+        start_times = [
+            section[0].timestamp for section in sections
+        ]  # the starting times of each section
         start_time = start_times[page - 1]  # get the starting time of the current page
+        # get the IDs of the dialog turns in the current page
+        dialog_turn_ids = [dialog_turn.id for dialog_turn in sections[page - 1]]
+        # annotation form for the client
+        form_client = PSAnnotationForm()
+        speaker = Speaker.client
+        # the condition below is True when the request method is POST and all validators pass
+        if form_client.validate_on_submit():
+            # save the annotations to the database
+            try:
+                new_dialog_turn_annotation_to_db(
+                    form_client, speaker, dataset_id, dialog_turn_ids
+                )
+                db.session.commit()
+            except:
+                db.session.rollback()
+        # annotation form for the therapist
+        form_therapist = PSAnnotationForm()
+        speaker = Speaker.therapist
+        # the condition below is True when the request method is POST and all validators pass
+        if form_therapist.validate_on_submit():
+            # save the annotations to the database
+            try:
+                new_dialog_turn_annotation_to_db(
+                    form_therapist, speaker, dataset_id, dialog_turn_ids
+                )
+                db.session.commit()
+            except:
+                db.session.rollback()
         return render_template(
             "annotate/annotate_ps.html",
             dataset_name=dataset.name,
