dfad58a74545a617f5d7be88fd101e6db1510555
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-08-03 15:44:55 +0100

    start working on test function for segment level annotations

--- tests/functional/test_annotate_ps.py
+++ tests/functional/test_annotate_ps.py
@@ -3,8 +3,19 @@ Functional tests for the `annotate` blueprint.
 Psychotherapy session dataset annotation page.
 """
 from flask_login import current_user
+from flask import url_for
 from bs4 import BeautifulSoup
 import pytest
+from app.utils import (
+    Speaker,
+    LabelStrength,
+    SubLabelsA,
+    SubLabelsB,
+    SubLabelsC,
+    SubLabelsD,
+    SubLabelsE,
+)
+from app.annotate.utils import split_dialog_turns
 
 
 @pytest.mark.dependency(
@@ -93,3 +104,60 @@ def test_annotate_ps_valid_login(test_client, insert_ps_dialog_turns):
     # log out
     response = test_client.get("/auth/logout", follow_redirects=True)
     assert response.status_code == 200
+
+
+@pytest.mark.dependency(
+    depends=["tests/unit/test_upload_parsers.py::test_psychotherapy_df_to_sql"],
+    scope="session",
+)
+def test_annotate_ps_valid_dialog_turn_annotation(test_client, insert_ps_dialog_turns):
+    """
+    GIVEN a Flask application configured for testing and a dataset with psychotherapy dialog turns
+    WHEN the '/annotate_psychotherapy' page is requested (POST) with valid dialog turn annotations
+    THEN check the response is valid and the annotations are saved to the database
+    """
+    # log in to the app
+    response = test_client.post(
+        "/auth/login",
+        data={"username": "annotator1", "password": "annotator1password"},
+        follow_redirects=True,
+    )
+    assert response.status_code == 200
+
+    # get the dataset id for the logged in user
+    dataset = current_user.datasets.filter_by(name="Psychotherapy Dataset Test").all()
+    dataset_id = dataset[0].id
+
+    # check that the "client" and "therapist" buttons are present in page 1
+    # these buttons are used to toggle the annotation forms
+    page = 1
+    url = url_for("annotate.annotate_ps", dataset_id=dataset_id, page=page)
+    response = test_client.get(url)
+    assert response.status_code == 200
+    soup = BeautifulSoup(response.data, "html.parser")
+    client_button = soup.find("button", id="btn_client")
+    assert client_button is not None
+    therapist_button = soup.find("button", id="btn_therapist")
+    assert therapist_button is not None
+
+    # submit an annotation for the client
+    client_data = {
+        "form_client.label_a": SubLabelsA.sublabel1,
+        "form_client.label_b": SubLabelsB.sublabel1,
+        "form_client.label_c": SubLabelsC.sublabel1,
+        "form_client.label_d": SubLabelsD.sublabel1,
+        "form_client.label_e": SubLabelsE.sublabel1,
+        "form_client.strength_a": LabelStrength.medium,
+        "form_client.strength_b": LabelStrength.medium,
+        "form_client.strength_c": LabelStrength.medium,
+        "form_client.strength_d": LabelStrength.medium,
+        "form_client.strength_e": LabelStrength.medium,
+        "form_client.comment_a": "test comment",
+        "form_client.comment_b": "test comment",
+        "form_client.comment_c": "test comment",
+        "form_client.comment_d": "test comment",
+        "form_client.comment_e": "test comment",
+        "speaker": Speaker.client,
+    }
+    response_client = test_client.post(url, data=client_data, follow_redirects=True)
+    assert response_client.status_code == 200
