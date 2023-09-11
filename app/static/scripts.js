f28943686e7b0ac3f1fd67c96ffa570c6750362e
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-09-11 12:08:31 +0100

    include button to toggle dyad form

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -8,8 +8,10 @@ $("#btn_client").on("click", function () {
     } else {
     // Button is not active, so activate it
     $("#form_therapist").collapse("hide"); // Hide therapist form
+    $("#form_dyad").collapse("hide"); // Hide dyad form
     $("#form_client").collapse("show"); // Show client form
     $("#btn_therapist").removeClass("active"); // Remove active class from therapist button
+    $("#btn_dyad").removeClass("active"); // Remove active class from dyad button
     $(this).addClass("active"); // Add active class to client button
     }
 });
@@ -21,9 +23,26 @@ $("#btn_therapist").on("click", function () {
     } else {
     // Button is not active, so activate it
     $("#form_client").collapse("hide"); // Hide client form
+    $("#form_dyad").collapse("hide"); // Hide dyad form
     $("#form_therapist").collapse("show"); // Show therapist form
     $("#btn_client").removeClass("active"); // Remove active class from client button
+    $("#btn_dyad").removeClass("active"); // Remove active class from dyad button
     $(this).addClass("active"); // Add active class to therapist button
+    }
+});
+$("#btn_dyad").on("click", function () {
+    if ($(this).hasClass("active")) {
+    // Button is already active, so deactivate it
+    $("#form_dyad").collapse("hide"); // Hide dyad form
+    $(this).removeClass("active"); // Remove active class from dyad button
+    } else {
+    // Button is not active, so activate it
+    $("#form_client").collapse("hide"); // Hide client form
+    $("#form_therapist").collapse("hide"); // Hide therapist form
+    $("#form_dyad").collapse("show"); // Show dyad form
+    $("#btn_client").removeClass("active"); // Remove active class from client button
+    $("#btn_therapist").removeClass("active"); // Remove active class from therapist button
+    $(this).addClass("active"); // Add active class to dyad button
     }
 });
 });
