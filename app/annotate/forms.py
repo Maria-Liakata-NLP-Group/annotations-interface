bb846a666cb2de3dc3a0781e87319a3f38700cea
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-09-20 16:22:15 +0100

    fix forms following split to annotations table

--- app/annotate/forms.py
+++ app/annotate/forms.py
@@ -99,72 +99,72 @@ def create_text_area_field(
 class PSAnnotationFormClient(FlaskForm):
     """Segment level annotation form of psychotherapy datasets for the client"""
 
-    label_a_client = create_select_field(
+    label_a = create_select_field(
         label=LabelNames.label_a_client.value,
         choices=SubLabelsAClient,
         name="label_a_client",
     )
-    label_b_client = create_select_field(
+    label_b = create_select_field(
         label=LabelNames.label_b_client.value,
         choices=SubLabelsBClient,
         name="label_b_client",
     )
-    label_c_client = create_select_field(
+    label_c = create_select_field(
         label=LabelNames.label_c_client.value,
         choices=SubLabelsCClient,
         name="label_c_client",
     )
-    label_d_client = create_select_field(
+    label_d = create_select_field(
         label=LabelNames.label_d_client.value,
         choices=SubLabelsDClient,
         name="label_d_client",
     )
-    label_e_client = create_select_field(
+    label_e = create_select_field(
         label=LabelNames.label_e_client.value,
         choices=SubLabelsEClient,
         name="label_e_client",
     )
-    label_f_client = create_select_field(
+    label_f = create_select_field(
         label=LabelNames.label_f_client.value,
         choices=SubLabelsFClient,
         name="label_f_client",
         default=SubLabelsFClient.no_change.name,
     )
-    strength_a_client = create_select_field(
+    strength_a = create_select_field(
         label="Strength", choices=LabelStrengthAClient, name="strength_a_client"
     )
-    strength_b_client = create_select_field(
+    strength_b = create_select_field(
         label="Strength", choices=LabelStrengthBClient, name="strength_b_client"
     )
-    strength_c_client = create_select_field(
+    strength_c = create_select_field(
         label="Strength", choices=LabelStrengthCClient, name="strength_c_client"
     )
-    strength_d_client = create_select_field(
+    strength_d = create_select_field(
         label="Strength", choices=LabelStrengthDClient, name="strength_d_client"
     )
-    strength_e_client = create_select_field(
+    strength_e = create_select_field(
         label="Strength", choices=LabelStrengthEClient, name="strength_e_client"
     )
-    strength_f_client = create_select_field(
+    strength_f = create_select_field(
         label="Strength",
         choices=LabelStrengthFClient,
         name="strength_f_client",
         default=LabelStrengthFClient.no_change.name,
     )
     comment_a = create_text_area_field(
-        label="Comment", name="comment_a_client", required_if="label_a_client"
+        label="Comment", name="comment_a_client", required_if="label_a"
     )
     comment_b = create_text_area_field(
-        label="Comment", name="comment_b_client", required_if="label_b_client"
+        label="Comment", name="comment_b_client", required_if="label_b"
     )
     comment_c = create_text_area_field(
-        label="Comment", name="comment_c_client", required_if="label_c_client"
+        label="Comment", name="comment_c_client", required_if="label_c"
     )
     comment_d = create_text_area_field(
-        label="Comment", name="comment_d_client", required_if="label_d_client"
+        label="Comment", name="comment_d_client", required_if="label_d"
     )
     comment_e = create_text_area_field(
-        label="Comment", name="comment_e_client", required_if="label_e_client"
+        label="Comment", name="comment_e_client", required_if="label_e"
     )
     comment_f = create_text_area_field(label="Comment", name="comment_f_client")
     comment_summary = create_text_area_field(
@@ -180,60 +180,60 @@ class PSAnnotationFormClient(FlaskForm):
 class PSAnnotationFormTherapist(FlaskForm):
     """Segment level annotation form of psychotherapy datasets for the therapist"""
 
-    label_a_therapist = create_select_field(
+    label_a = create_select_field(
         label=LabelNames.label_a_therapist.value,
         choices=SubLabelsATherapist,
         name="label_a_therapist",
     )
-    label_b_therapist = create_select_field(
+    label_b = create_select_field(
         label=LabelNames.label_b_therapist.value,
         choices=SubLabelsBTherapist,
         name="label_b_therapist",
     )
-    label_c_therapist = create_select_field(
+    label_c = create_select_field(
         label=LabelNames.label_c_therapist.value,
         choices=SubLabelsCTherapist,
         name="label_c_therapist",
     )
-    label_d_therapist = create_select_field(
+    label_d = create_select_field(
         label=LabelNames.label_d_therapist.value,
         choices=SubLabelsDTherapist,
         name="label_d_therapist",
     )
-    label_e_therapist = create_select_field(
+    label_e = create_select_field(
         label=LabelNames.label_e_therapist.value,
         choices=SubLabelsETherapist,
         name="label_e_therapist",
     )
-    strength_a_therapist = create_select_field(
+    strength_a = create_select_field(
         label="Strength", choices=LabelStrengthATherapist, name="strength_a_therapist"
     )
-    strength_b_therapist = create_select_field(
+    strength_b = create_select_field(
         label="Strength", choices=LabelStrengthBTherapist, name="strength_b_therapist"
     )
-    strength_c_therapist = create_select_field(
+    strength_c = create_select_field(
         label="Strength", choices=LabelStrengthCTherapist, name="strength_c_therapist"
     )
-    strength_d_therapist = create_select_field(
+    strength_d = create_select_field(
         label="Strength", choices=LabelStrengthDTherapist, name="strength_d_therapist"
     )
-    strength_e_therapist = create_select_field(
+    strength_e = create_select_field(
         label="Strength", choices=LabelStrengthETherapist, name="strength_e_therapist"
     )
     comment_a = create_text_area_field(
-        label="Comment", name="comment_a_therapist", required_if="label_a_therapist"
+        label="Comment", name="comment_a_therapist", required_if="label_a"
     )
     comment_b = create_text_area_field(
-        label="Comment", name="comment_b_therapist", required_if="label_b_therapist"
+        label="Comment", name="comment_b_therapist", required_if="label_b"
     )
     comment_c = create_text_area_field(
-        label="Comment", name="comment_c_therapist", required_if="label_c_therapist"
+        label="Comment", name="comment_c_therapist", required_if="label_c"
     )
     comment_d = create_text_area_field(
-        label="Comment", name="comment_d_therapist", required_if="label_d_therapist"
+        label="Comment", name="comment_d_therapist", required_if="label_d"
     )
     comment_e = create_text_area_field(
-        label="Comment", name="comment_e_therapist", required_if="label_e_therapist"
+        label="Comment", name="comment_e_therapist", required_if="label_e"
     )
     comment_summary = create_text_area_field(
         label="Summary Comment",
@@ -248,23 +248,23 @@ class PSAnnotationFormTherapist(FlaskForm):
 class PSAnnotationFormDyad(FlaskForm):
     """Segment level annotation form of psychotherapy datasets for the dyad"""
 
-    label_a_dyad = create_select_field(
+    label_a = create_select_field(
         label=LabelNames.label_a_dyad.value, choices=SubLabelsADyad, name="label_a_dyad"
     )
-    label_b_dyad = create_select_field(
+    label_b = create_select_field(
         label=LabelNames.label_b_dyad.value, choices=SubLabelsBDyad, name="label_b_dyad"
     )
-    strength_a_dyad = create_select_field(
+    strength_a = create_select_field(
         label="Strength", choices=LabelStrengthADyad, name="strength_a_dyad"
     )
-    strength_b_dyad = create_select_field(
+    strength_b = create_select_field(
         label="Strength", choices=LabelStrengthBDyad, name="strength_b_dyad"
     )
     comment_a = create_text_area_field(
-        label="Comment", name="comment_a_dyad", required_if="label_a_dyad"
+        label="Comment", name="comment_a_dyad", required_if="label_a"
     )
     comment_b = create_text_area_field(
-        label="Comment", name="comment_b_dyad", required_if="label_b_dyad"
+        label="Comment", name="comment_b_dyad", required_if="label_b"
     )
     comment_summary = create_text_area_field(
         label="Summary Comment",
