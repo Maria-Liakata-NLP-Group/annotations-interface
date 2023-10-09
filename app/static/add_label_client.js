17dab5b6b00efa8461aaf8d5256d89cb87eed8b8
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-10-09 13:00:04 +0100

    add css and js to prettier pre-commit hook

--- app/static/add_label_client.js
+++ app/static/add_label_client.js
@@ -1,11 +1,11 @@
 // Function to toggle an additional form group for each label for client annotations
 $(document).ready(function () {
-    $("#btn_client_label_a_add").click(function () {
-        $("#client_label_a_add").toggle();
-        if ($(this).hasClass("active")) {
-            $(this).removeClass("active");
-        } else {
-            $(this).addClass("active");
-        }
-    });
+  $("#btn_client_label_a_add").click(function () {
+    $("#client_label_a_add").toggle();
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+    } else {
+      $(this).addClass("active");
+    }
+  });
 });
