2e92a26fbb63eb709cc8e443cd52dac5c3c2375c
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-10-11 17:44:54 +0100

    update java script to handle new button behavior

--- app/static/psychotherapy_form_buttons.js
+++ app/static/psychotherapy_form_buttons.js
@@ -1,48 +1,23 @@
-// Function to show/hide client, therapist and dyad annotation forms at segment level
 $(document).ready(function () {
   $("#btn_client").on("click", function () {
-    if ($(this).hasClass("active")) {
-      // Button is already active, so deactivate it
-      $("#form_client").collapse("hide"); // Hide client form
-      $(this).removeClass("active"); // Remove active class from client button
-    } else {
-      // Button is not active, so activate it
-      $("#form_therapist").collapse("hide"); // Hide therapist form
-      $("#form_dyad").collapse("hide"); // Hide dyad form
-      $("#form_client").collapse("show"); // Show client form
-      $("#btn_therapist").removeClass("active"); // Remove active class from therapist button
-      $("#btn_dyad").removeClass("active"); // Remove active class from dyad button
-      $(this).addClass("active"); // Add active class to client button
-    }
+    var url = $(this).attr("form-url");
+    window.location.href = url; // Redirect to client form
+    $("#btn_therapist").removeClass("active"); // Remove active class from therapist button
+    $("#btn_dyad").removeClass("active"); // Remove active class from dyad button
+    $(this).addClass("active"); // Add active class to client button
   });
   $("#btn_therapist").on("click", function () {
-    if ($(this).hasClass("active")) {
-      // Button is already active, so deactivate it
-      $("#form_therapist").collapse("hide"); // Hide therapist form
-      $(this).removeClass("active"); // Remove active class from therapist button
-    } else {
-      // Button is not active, so activate it
-      $("#form_client").collapse("hide"); // Hide client form
-      $("#form_dyad").collapse("hide"); // Hide dyad form
-      $("#form_therapist").collapse("show"); // Show therapist form
-      $("#btn_client").removeClass("active"); // Remove active class from client button
-      $("#btn_dyad").removeClass("active"); // Remove active class from dyad button
-      $(this).addClass("active"); // Add active class to therapist button
-    }
+    var url = $(this).attr("form-url");
+    window.location.href = url; // Redirect to therapist form
+    $("#btn_client").removeClass("active"); // Remove active class from client button
+    $("#btn_dyad").removeClass("active"); // Remove active class from dyad button
+    $(this).addClass("active"); // Add active class to therapist button
   });
   $("#btn_dyad").on("click", function () {
-    if ($(this).hasClass("active")) {
-      // Button is already active, so deactivate it
-      $("#form_dyad").collapse("hide"); // Hide dyad form
-      $(this).removeClass("active"); // Remove active class from dyad button
-    } else {
-      // Button is not active, so activate it
-      $("#form_client").collapse("hide"); // Hide client form
-      $("#form_therapist").collapse("hide"); // Hide therapist form
-      $("#form_dyad").collapse("show"); // Show dyad form
-      $("#btn_client").removeClass("active"); // Remove active class from client button
-      $("#btn_therapist").removeClass("active"); // Remove active class from therapist button
-      $(this).addClass("active"); // Add active class to dyad button
-    }
+    var url = $(this).attr("form-url");
+    window.location.href = url; // Redirect to dyad form
+    $("#btn_client").removeClass("active"); // Remove active class from client button
+    $("#btn_therapist").removeClass("active"); // Remove active class from therapist button
+    $(this).addClass("active"); // Add active class to dyad button
   });
 });
