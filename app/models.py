373892210f0af03182178c9388138b16529dba81
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-10-25 17:33:37 +0100

    table for therapist annotation schema scales

--- app/models.py
+++ app/models.py
@@ -682,6 +682,12 @@ class TherapistAnnotationSchema(db.Model):
         backref=db.backref("parent", remote_side=[id]),
         lazy="dynamic",
     )
+    scales = db.relationship(
+        "TherapistAnnotationSchemaScale",
+        backref="label",
+        lazy="dynamic",
+    )  # one-to-many relationship with TherapistAnnotationSchemaScale class
+
     # create unique constraint on label within a parent
     __table_args__ = (db.UniqueConstraint("label", "parent_id"),)
 
@@ -731,6 +737,27 @@ class ClientAnnotationSchemaScale(db.Model):
     def __repr__(self):
         """How to print objects of this class"""
         return "<ClientAnnotationSchemaScale {}>".format(self.scale_title[:10])
+
+
+class TherapistAnnotationSchemaScale(db.Model):
+    """Table to store the scales for the therapist annotation schema"""
+
+    __tablename__ = "therapist_annotation_schema_scale"
+
+    id = db.Column(db.Integer, primary_key=True)
+    scale_title = db.Column(db.String(64), index=True, nullable=False)
+    scale_level = db.Column(db.String(64), index=True, nullable=False)
+    id_therapist_annotation_schema = db.Column(
+        db.Integer, db.ForeignKey("therapist_annotation_schema.id")
+    )
+    # create unique constraint on scale_title and id_therapist_annotation_schema
+    __table_args__ = (
+        db.UniqueConstraint("scale_title", "id_therapist_annotation_schema"),
+    )
+
+    def __repr__(self):
+        """How to print objects of this class"""
+        return "<TherapistAnnotationSchemaScale {}>".format(self.scale_title[:10])
 
 
 class AnnotationSchemaManager:
