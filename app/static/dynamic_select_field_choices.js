88e6bac915c6fd76aebc7a1b23930dbc02c90ac1
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-11-09 16:11:39 +0000

    java script to update choices of select field based on selection

---
+++ app/static/dynamic_select_field_choices.js
@@ -0,0 +1,28 @@
+// JavaScript to dynamically update the choices of a second select field
+// based on the choice of another select field.
+
+$(document).ready(function () {
+  // find all select fields with the class "dynamic_select"
+  $(".dynamic_select").each(function () {
+    var firstSelect = $(this);
+    var targetId = firstSelect.data("target");
+    var secondSelect = $("#" + targetId);
+
+    firstSelect.on("change", function () {
+      var selectedValue = firstSelect.val();
+      // make an AJAX request to the server to get the updated
+      // choices for the second select field
+      $.ajax({
+        url: "/update_select_choices",
+        type: "POST",
+        data: {
+          selected_value: selectedValue,
+        },
+        success: function (data) {
+          // update the second select field with the new choices
+          secondSelect.html(data.options);
+        },
+      });
+    });
+  });
+});
