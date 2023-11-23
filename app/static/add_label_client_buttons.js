b7c9b539b9ffd6e56dda45bcab701f20f8afa7f2
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-11-23 16:51:22 +0000

    refactor JS to toggle forms as a function and make evidence select field optional or required

--- app/static/add_label_client_buttons.js
+++ app/static/add_label_client_buttons.js
@@ -1,43 +1,48 @@
 // Function to toggle an additional form group for each label for client annotations
+// It also makes the evidence field compulsory when the form group is visible
+
+function toggleAdditionalFormGroup(buttonId, formGroupId, evidenceId) {
+  $(buttonId).click(function () {
+    $(formGroupId).toggle();
+    if ($(this).hasClass("active")) {
+      // if the toggle button is active...
+      $(this).removeClass("active");
+      // make the evidence field optional when the form group is hidden
+      $(evidenceId).prop("required", false);
+    } else {
+      // if the toggle button is not active...
+      $(this).addClass("active");
+      // make the evidence field compulsory when the form group is visible
+      $(evidenceId).prop("required", true);
+    }
+  });
+}
+
+// call the function
 $(document).ready(function () {
-  $("#btn_client_label_a_add").click(function () {
-    $("#client_label_a_add").toggle();
-    if ($(this).hasClass("active")) {
-      $(this).removeClass("active");
-    } else {
-      $(this).addClass("active");
-    }
-  });
-  $("#btn_client_label_b_add").click(function () {
-    $("#client_label_b_add").toggle();
-    if ($(this).hasClass("active")) {
-      $(this).removeClass("active");
-    } else {
-      $(this).addClass("active");
-    }
-  });
-  $("#btn_client_label_c_add").click(function () {
-    $("#client_label_c_add").toggle();
-    if ($(this).hasClass("active")) {
-      $(this).removeClass("active");
-    } else {
-      $(this).addClass("active");
-    }
-  });
-  $("#btn_client_label_d_add").click(function () {
-    $("#client_label_d_add").toggle();
-    if ($(this).hasClass("active")) {
-      $(this).removeClass("active");
-    } else {
-      $(this).addClass("active");
-    }
-  });
-  $("#btn_client_label_e_add").click(function () {
-    $("#client_label_e_add").toggle();
-    if ($(this).hasClass("active")) {
-      $(this).removeClass("active");
-    } else {
-      $(this).addClass("active");
-    }
-  });
+  toggleAdditionalFormGroup(
+    "#btn_client_label_a_add",
+    "#client_label_a_add",
+    "#evidence_a_client_add"
+  );
+  toggleAdditionalFormGroup(
+    "#btn_client_label_b_add",
+    "#client_label_b_add",
+    "#evidence_b_client_add"
+  );
+  toggleAdditionalFormGroup(
+    "#btn_client_label_c_add",
+    "#client_label_c_add",
+    "#evidence_c_client_add"
+  );
+  toggleAdditionalFormGroup(
+    "#btn_client_label_d_add",
+    "#client_label_d_add",
+    "#evidence_d_client_add"
+  );
+  toggleAdditionalFormGroup(
+    "#btn_client_label_e_add",
+    "#client_label_e_add",
+    "#evidence_e_client_add"
+  );
 });
