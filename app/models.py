0e0ce766d406b9cf1f8e22e752ff0a14e204ad52
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-12-07 11:28:55 +0000

    Revert "update table to store client annotation comments"
    This reverts commit b4b79de6fe198c2ab26048270d12eebbe324d722.

--- app/models.py
+++ app/models.py
@@ -89,11 +89,6 @@ class ClientAnnotationSchemaAssociation(db.Model):
         "PSAnnotationClient",
         back_populates="annotation_label_associations",
     )
-    comment = db.relationship(
-        "ClientAnnotationComment",
-        backref="association",
-        lazy="dynamic",
-    )  # one-to-many relationship with ClientAnnotationComment class
 
     def __init__(self, label=None, annotation=None, is_additional=False):
         self.label = label
@@ -530,6 +525,11 @@ class PSAnnotationClient(db.Model):
         backref=db.backref("annotations", lazy="dynamic"),
         lazy="dynamic",
     )  # many-to-many relationship with ClientAnnotationSchemaScale class
+    annotation_comments = db.relationship(
+        "ClientAnnotationComment",
+        backref="annotation",
+        lazy="dynamic",
+    )  # one-to-many relationship with ClientAnnotationComment class
 
 
 class PSAnnotationTherapist(db.Model):
@@ -919,6 +919,11 @@ class ClientAnnotationSchema(db.Model, AnnotationSchemaMixin):
         backref="label",
         lazy="dynamic",
     )  # one-to-many relationship with ClientAnnotationSchemaScale class
+    comments = db.relationship(
+        "ClientAnnotationComment",
+        backref="label",
+        lazy="dynamic",
+    )  # one-to-many relationship with ClientAnnotationComment class
     # create unique constraint on label within a parent
     __table_args__ = (db.UniqueConstraint("label", "parent_id"),)
 
@@ -1060,14 +1065,17 @@ class DyadAnnotationSchemaScale(db.Model):
 
 
 class ClientAnnotationComment(db.Model):
-    """Table to store the comments for the client annotations"""
+    """Table to store the comments for the client annotation schema"""
 
     __tablename__ = "client_annotation_comment"
 
     id = db.Column(db.Integer, primary_key=True)
     comment = db.Column(db.Text, nullable=True)
-    id_annotationclient_annotationschema = db.Column(
-        db.Integer, db.ForeignKey("annotationclient_annotationschema.id")
+    id_client_annotation_schema = db.Column(
+        db.Integer, db.ForeignKey("client_annotation_schema.id")
+    )
+    id_ps_annotation_client = db.Column(
+        db.Integer, db.ForeignKey("ps_annotation_client.id")
     )
 
     def __repr__(self):
